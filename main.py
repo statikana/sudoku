16877534f74d574e572e4303a31e79a84760c8cc
statikana <86629697+statikana@users.noreply.github.com>
2024-09-25 20:13:29 -0400

    fix create boolean mapping of valid pairs for double seeking

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ class Sudoku:
         self.box_height: int = 3
         self.box_width: int = 3
 
-    def is_solved(self) -> bool:
+    def is_filled(self) -> bool:
         """
         Whether or not all spots in the board have an answer placed
         """
         return all(all(row) for row in self.board)
+    
+    def is_valid(self) -> bool:
+        for row, col in product(range(self.height), range(self.width)):
+            value = self.board[row, col]
+            inter = self.get_interacting_indicies(row, col)
+            for i, j in inter:
+                if self.board[i, j] == value:
+                    print(f"val at {row} {col} hits val {i}, {j} [value: {value}]")
+        
+        return True
 
     def get_box_values(self, n: int) -> npt.NDArray[np.int8]:
         """
@@ -81,9 +91,14 @@ class Sudoku:
         return (rows | cols | box) - {(row, col)}
 
     def solve(self):
-        self.singles_solve()  # solves using singles until no change can be made
-        self.doubles_pass() # solves using doubles
+        print(self.possibility_counts())
         self.singles_solve()
+        print(self.possibility_counts())
+        self.doubles_pass()
+        print(self.possibility_counts())
+        self.singles_solve()
+        print(self.possibility_counts())
+        
         
     def singles_pass(self, possible: npt.NDArray[np.int8] | None = None):        
         if possible is None:
@@ -91,7 +106,7 @@ class Sudoku:
         self.replace_singles(possible)
     
     def singles_solve(self):
-        while not self.is_solved():
+        while not self.is_filled():
             old = deepcopy(self.board)
             self.singles_pass()
             if np.all(old == self.board):
@@ -122,10 +137,12 @@ class Sudoku:
         
     def resolve_pairs(self, possible: npt.NDArray):
             
-        has_two_boolmap: npt.NDArray = sum(possible) == 2  # 9x9 array # type: ignore
+        has_two_boolmap = self.create_has_two_boolmap()
         for row, col in product(range(self.height), range(self.width)):
             if not has_two_boolmap[row, col]:
                 continue
+            else:
+                print("has 2", possible[row, col], (row, col))
             
             interacting = self.get_interacting_indicies(row, col)  # all ones in the same row, col, or box
             for irow, icol in interacting:
@@ -134,20 +151,42 @@ class Sudoku:
                     for context in contexts:
                         match context:
                             case InteractionContext.ROW:
-                                indicies = set(product([row] * 9, range(9)))
+                                indicies = {(row, i_col) for i_col in range(9)}
                                 
                             case InteractionContext.COL:
-                                indicies = set(product(range(9), [col] * 9))
+                                indicies = {(i_row, col) for i_row in range(9)}
                                 
                             case InteractionContext.BOX:
                                 base_row, base_col = row - row%3, col - col%3  # top left of block
                                 indicies = set(product(range(base_row, base_row+3), range(base_col, base_col+3)))
                         
                         indicies -= {(row, col), (irow, icol)}
-                        known_map = possible[row, col]
+                        known_map = possible[row, col].astype(bool)  # is also the same as possible[irow, icol]
                         for i, j in indicies:
-                            possible[i, j] = (possible[i, j].astype(bool) | known_map.astype(bool)) & ~(possible[i, j].astype(bool) & known_map.astype(bool))
+                            # [1, 1, 0, 0, 0, 0, 1, 0]  # < something else in the same row "this_possible"
+                            # [0, 1, 1, 0, 0, 0, 0, 0]  # < trying to remove "known_map"
+                            
+                            # [1, 0, 0, 0, 0, 0, 1, 0] $ desired
+                            # [1, 0, 1, 0, 0, 0, 1, 0] # xor
+                            #        ^
+                            this_possible = possible[i, j].astype(bool)
+                            print("this", this_possible)
+                            print("knwn", known_map)
+                            possible[i, j] = ((this_possible | known_map) & ~(this_possible & known_map)) & this_possible
+                            print("newp", possible[i, j].astype(bool))
+                            print()
         return possible
+    
+    def create_has_two_boolmap(self):
+        possible = self.create_possible()
+        start = np.zeros_like(self.board)  # eg. 9x9
+        
+        for row, col in product(range(self.height), range(self.width)):
+            possible_list = list(possible[row, col])  # 9-value boolean-like array for each digit's possibility of being in this square
+            if sum(possible_list) == 2:
+                start[row, col] = True
+        
+        return start
     
     def get_row(self, row_n: int):
         return self.board[row_n]
@@ -241,10 +280,12 @@ class InteractionContext(Enum):
     COL = 1
     BOX = 2
 
-sudoku = Sudoku(easy_1) 
+sudoku = Sudoku(medium_1) 
 print(sudoku)
 sudoku.solve()
 print(sudoku)
+print(sudoku.is_filled(), sudoku.is_valid())
+
 # find 2 boxes with matching pairs of possible values
 # that are intersecting
 # subtract from other intersecting boxes in the same intersection context (row, col, box)
